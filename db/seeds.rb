5b117355cb1ce2e11eb6d3f47fec191c2d93d909
Louis Bemberg <louis.bemberg@gmail.com>
2020-06-10 14:49:36 +0200

    Finished seeding 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts '1 - Erasing all current restaurants'
+Restaurant.destroy_all
+
+puts '2 - creating new set of Restaurants'
+first_resto = { name: "Lou's Burger Shack", address: "Lavaux 28, 1009 Pully, Suisse", category: "italian"}
+second_resto = { name: "Caro's Delicious Burritos", address: "BikeLane 66, 1506 Amsterdam, Netherlands", category: "belgian"}
+third_resto = { name: "Nik's Beer Club", address: "BeerStrasse, 2424 Berlin, Germany", category: "japanese"}
+fourth_resto = { name: "Hassan Burger Spot", address: "Marbella, Spain", category: "chinese"}
+fifth_resto = { name: "Come's Surfer Corner", address: "Beach, Barcelona, Spain", category: "french"}
+
+resto_array = [first_resto, second_resto, third_resto, fourth_resto, fifth_resto]
+
+counter = 2
+
+resto_array.each do |resto|
+  restaurant = Restaurant.create!(resto)
+  puts "#{counter += 1} - Created #{restaurant.name}"
+end
+
+puts "#{counter += 1} - Seeding Finished! :)"
